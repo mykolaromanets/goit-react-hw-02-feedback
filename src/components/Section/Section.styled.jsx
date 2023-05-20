04e7d9c33a5f0e7cc3bd6fb35a8ced17c2b94830
mykolaromanets <125486364+mykolaromanets@users.noreply.github.com>
2023-05-20 23:40:55 +0300

    finished

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SectionContainer = styled.section`
+  margin: auto;
+  padding: 20px;
+  width: 400px;
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+  margin: 0;
+`;
